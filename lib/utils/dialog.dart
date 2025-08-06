4d8f51ad3a2f6c50ad95d714550f36b24528b4b5
App攻城狮 <1215933389@qq.com>
2025-08-06 15:17:45 +0800

    游戏暂停弹窗
    游戏暂停弹窗

---
+++ lib/utils/dialog.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class TTDialog {
+  //*游戏暂停弹窗*/
+  static gamePauseTaskDialog(BuildContext context, Function exchange) {
+    showDialog(
+        context: context,
+        barrierColor: Color.fromRGBO(18, 83, 157, 1.0),
+        builder: (BuildContext context) {
+          return Dialog.fullscreen(
+            backgroundColor: Color.fromARGB(18, 83, 157, 1),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height:MediaQuery.of(context).size.height,
+              child: GamePauseTaskDialog(exchange: exchange),
+            ),
+          );
+        }
+    );
+  }
+}
+
+
+//*游戏暂停弹窗*/
+class GamePauseTaskDialog extends StatelessWidget {
+  Function exchange;
+  GamePauseTaskDialog({required this.exchange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(18, 83, 157, 1.0),
+      width: MediaQuery.of(context).size.width,
+      height:MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // SizedBox(
+          //   height: 90,
+          // ),
+          Constants.boldWhiteTextWidget('', 36),
+
+          Container(
+            child: Column(
+              children: [
+                Constants.boldWhiteTextWidget('Pause', 36),
+                SizedBox(
+                  height: 6,
+                ),
+                Container(
+                  width: 65,
+                  height: 27,
+                  color: Color.fromRGBO(12, 51, 109, 1.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/arrow.png",width: 8,height: 10,),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Constants.boldWhiteTextWidget('Start', 12,fontColor: Color.fromRGBO(21, 233, 120, 1.0)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+
+          Container(
+            // color: Colors.red,
+            margin: EdgeInsets.only(bottom: 40,left: 40,right: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 127,
+                  height: 55,
+                  color: Color.fromRGBO(100, 100, 100, 1.0),
+                  child: GestureDetector(onTap: (){
+                    print("退出游戏");
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                    },
+                    child:
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/stop.png',
+                          width: 26/2,
+                          height: 22/2,
+                        ),
+                        SizedBox(width: 11,),
+                        Constants.boldWhiteTextWidget("Exit", 20),
+
+                      ],
+                    ),
+                  ),
+
+
+                ),
+                Container(
+                    // margin: EdgeInsets.only(right: 40,bottom: 40),
+                    width: 127,
+                    height: 55,
+                    color: Color.fromRGBO(100, 100, 100, 0.7),
+                    child: GestureDetector(onTap: (){
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                      /// 机器人位置校准回到原点
+                      // CommStatusManager().writerData(positionCheckData());
+                    },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/stop.png',
+                            width: 26/2,
+                            height: 22/2,
+                          ),
+                          SizedBox(width: 11,),
+                          Constants.boldWhiteTextWidget("Replay", 20),
+                        ],
+                      ),
+                    )
+                )
+              ],
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
